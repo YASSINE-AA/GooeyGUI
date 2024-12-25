eaf549eb74dc2f44fd8d958aefe3adcec26dec50
YASSINE-AA <overflowtn@gmail.com>
2024-12-25 19:12:44 +0100

    Remove website submodule

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yassine-ahmed-ali/Documents/GooeyGUI_refactored
-# Build directory: /home/yassine-ahmed-ali/Documents/GooeyGUI_refactored/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("third_party/glfw")
-subdirs("third_party/cjson")
-subdirs("third_party/freetype")
--- build/third_party/cjson/fuzzing/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yassine-ahmed-ali/Documents/GooeyGUI_refactored/third_party/cjson/fuzzing
-# Build directory: /home/yassine-ahmed-ali/Documents/GooeyGUI_refactored/build/third_party/cjson/fuzzing
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
