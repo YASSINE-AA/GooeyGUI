835c265d71bb06fcb0681b4b72ff7c244e802383
YASSINE-AA <overflowtn@gmail.com>
2024-12-27 22:40:44 +0100

    .gitignore updated

--- test_app/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yassine-ahmed-ali/Documents/GooeyGUI
-# Build directory: /home/yassine-ahmed-ali/Documents/GooeyGUI/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("third_party/glfw")
-subdirs("third_party/cjson")
-subdirs("third_party/freetype")
